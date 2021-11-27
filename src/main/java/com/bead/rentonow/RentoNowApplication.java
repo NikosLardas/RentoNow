4adce670b94d49380757ea07c3dc70c6cb5b7f86
NikosLardas <nilardas@yahoo.gr>
2021-11-27 20:46:34 +0200

    Initial commit

---
+++ src/main/java/com/bead/rentonow/RentoNowApplication.java
@@ -0,0 +1,13 @@
+package com.bead.rentonow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentoNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentoNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bead/rentonow/RentoNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bead.rentonow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentoNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
